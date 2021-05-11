045960f5483a812da8d09f53df3373ee5809a24c
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-05-11 20:38:35 +0200

    added logic for Pokedex component

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,13 +1,45 @@
 import React, {
     Component
 } from 'react';
+import Pokecard from './Pokecard'
 import pokeCollection from './pokeCollection'
+import './Pokedex.css'
 
 
 class Pokedex extends Component {
     render() {
-        return
+        return ( 
+        <div className = "Pokedex" >
+            <h1 className = "Pokedex-title" > Pokedex </h1> 
+            <div className="Pokecard-wrap">
+                <Pokecard name = "Eevee"
+                img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png"
+                type = "normal"
+                exp = "65" />
+                <Pokecard name = "Eevee"
+                img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png"
+                type = "normal"
+                exp = "65" />
+                <Pokecard name = "Eevee"
+                img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png"
+                type = "normal"
+                exp = "65" />
+                <Pokecard name = "Eevee"
+                img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png"
+                type = "normal"
+                exp = "65" />
+                <Pokecard name = "Eevee"
+                img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png"
+                type = "normal"
+                exp = "65" />
+                <Pokecard name = "Eevee"
+                img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png"
+                type = "normal"
+                exp = "65" />
+            </div>
+            </div>
+        )
     }
 };
 
-export default Pokedex
+export default Pokedex;
