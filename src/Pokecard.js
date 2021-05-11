6435217bb1ff2affb79320ae7ee07501fe2bfbbf
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-05-11 19:59:26 +0200

    created own starter files

---
+++ src/Pokecard.js
@@ -0,0 +1,13 @@
+import React, {
+    Component
+} from 'react';
+
+class Pokecard extends Component {
+    render() {
+        return (
+
+        )
+    }
+};
+
+export default Pokecard;
---
+++ src/Pokedex.js
@@ -0,0 +1,13 @@
+import React, {
+    Component
+} from 'react';
+import pokeCollection from './pokeCollection'
+
+
+class Pokedex extends Component {
+    render() {
+        return
+    }
+};
+
+export default Pokedex
