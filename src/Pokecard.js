302c4f9bf676355fa1781d1f0f8c2e8526161921
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-05-12 21:33:36 +0200

    added logic for adding zeros on short id`s

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -5,12 +5,18 @@ import './Pokecard.css'
 
 class Pokecard extends Component {
     render() {
-        const {name, img, type, exp} = this.props;
+        function addZeros(num, targetLength){
+            return num.toString().padStart(targetLength, 0)
+        }
 
+        const {name, type, exp} = this.props;
+        let { id } = this.props;
+
+        const idEdit = addZeros(id, 3)
         return (
                 <div className="Pokecard">
                     <h3 className="Pokecard-name">{name}</h3>
-                    <img className="Pokecard-img" src={img}></img>
+                    <img className="Pokecard-img" src={"https://assets.pokemon.com/assets/cms2/img/pokedex/detail/"+ idEdit +".png"}></img>
                     <p className="Pokecard-type">Type: {type}</p>
                     <p className="Pokecard-exp">EXP: {exp}</p>
                 </div>
