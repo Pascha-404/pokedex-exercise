a659369240f44c8f6f6604c1cdcb253eae26eaa5
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-05-12 21:32:57 +0200

    made pokemon through array available

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -8,34 +8,14 @@ import './Pokedex.css'
 
 class Pokedex extends Component {
     render() {
+        const renderPokemon = pokeCollection.map(pokemon => (
+            <Pokecard name={pokemon.name} id={pokemon.id} type={pokemon.type} exp={pokemon.base_experience} />
+        ))
         return ( 
         <div className = "Pokedex" >
             <h1 className = "Pokedex-title" > Pokedex </h1> 
             <div className="Pokecard-wrap">
-                <Pokecard name = "Eevee"
-                img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png"
-                type = "normal"
-                exp = "65" />
-                <Pokecard name = "Eevee"
-                img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png"
-                type = "normal"
-                exp = "65" />
-                <Pokecard name = "Eevee"
-                img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png"
-                type = "normal"
-                exp = "65" />
-                <Pokecard name = "Eevee"
-                img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png"
-                type = "normal"
-                exp = "65" />
-                <Pokecard name = "Eevee"
-                img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png"
-                type = "normal"
-                exp = "65" />
-                <Pokecard name = "Eevee"
-                img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png"
-                type = "normal"
-                exp = "65" />
+                { renderPokemon }
             </div>
             </div>
         )
