181e2ac872ada1fe81ec9a3bb31c0eac18a55e67
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-05-13 16:17:23 +0200

    logic for score and looser/winner

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,21 +2,31 @@ import React, {
     Component
 } from 'react';
 import Pokecard from './Pokecard'
-import pokeCollection from './pokeCollection'
 import './Pokedex.css'
 
 
 class Pokedex extends Component {
     render() {
-        const renderPokemon = pokeCollection.map(pokemon => (
+        const { hand, handScore } = this.props;
+        const renderPokemon = hand.map(pokemon => (
             <Pokecard name={pokemon.name} id={pokemon.id} type={pokemon.type} exp={pokemon.base_experience} />
-        ))
+        ));
+        let classes;
+        let announcement        
+        if(this.props.isWinner){
+            classes = "Pokedex winner"
+            announcement = "THIS HAND WINS!"
+        } else{
+            classes = "Pokedex looser"
+        };
         return ( 
-        <div className = "Pokedex" >
+        <div className = {classes} >
             <h1 className = "Pokedex-title" > Pokedex </h1> 
+            <h3 className = "Pokedex-totalExp">Total EXP: {handScore}</h3>
             <div className="Pokecard-wrap">
                 { renderPokemon }
             </div>
+            <p>{announcement}</p>
             </div>
         )
     }
