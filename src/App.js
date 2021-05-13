9cc9417b0b9fd5d17a864604ac695512eaf1a40f
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-05-13 16:16:12 +0200

    changed Pokedex with Pokegame component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, {
     Component
 } from 'react';
-import Pokedex from './Pokedex'
+import Pokegame from './Pokegame'
 
 class App extends Component {
     render() {
         return (
             
             <div className="App">
-                <Pokedex />
+                <Pokegame />
             </div>
         )
         
