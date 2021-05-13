2928f763310e98e0ef70f2ef8a4c1dbf21cbb705
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-05-13 13:59:23 +0200

    starter file

---
+++ src/Pokegame.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import Pokedex from './Pokedex';
+import pokeCollection from './pokeCollection'
+
+class Pokegame extends Component {
+render(){
+    return()
+}
+}
+
+export default Pokegame
