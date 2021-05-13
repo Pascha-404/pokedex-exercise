a3fcd7492b80eed701062a2462f6568c1b22e4b1
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-05-13 16:17:59 +0200

    logic for randomPokemon/calcScore/isWinner

--- src/Pokegame.js
+++ src/Pokegame.js
@@ -4,7 +4,46 @@ import pokeCollection from './pokeCollection'
 
 class Pokegame extends Component {
 render(){
-    return()
+    function randomPokemon(array){
+        let pickedPokemon = []
+        for(let i = 0; i <= 3; i++){
+            pickedPokemon.push(array[Math.trunc(Math.random() * array.length)])
+        }
+        return pickedPokemon
+    };
+
+    function calcExp(array){
+        let sum = 0;
+        array.forEach(pokemon => {
+            sum = pokemon.base_experience + sum
+        });
+        return sum
+    };
+    
+    function isWinner(firstScore, secondScore){
+        if(firstScore > secondScore){
+            return <div className="Pokegame">
+            <Pokedex hand={firstHand} handScore={firstHandScore} isWinner />
+            <Pokedex hand={secondHand} handScore={secondHandScore} />
+        </div>
+        } else {
+            return <div className="Pokegame">
+            <Pokedex hand={firstHand} handScore={firstHandScore} />
+            <Pokedex hand={secondHand} handScore={secondHandScore} isWinner />
+        </div>
+        }
+    }
+
+    const firstHand = randomPokemon(pokeCollection)
+    const secondHand = randomPokemon(pokeCollection)
+    const firstHandScore = calcExp(firstHand);
+    const secondHandScore = calcExp(secondHand);
+
+    const game = isWinner(firstHandScore, secondHandScore)
+
+    return(
+        game
+    )
 }
 }
 
